3f5fa97a25f8b2713d2ded750e11bdc2030be031
Miguel Alejandro Santiesteban Aguiar <miguel.santiesteban.aguiar@gmail.com>
2025-10-10 17:56:23 +0100

    all

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button.astro';
+export { default as Chip } from './Chip.astro';
+export { default as Card } from './Card.astro';
+export { default as Container } from './Container.astro';
+export { default as Carousel } from './Carousel.astro';
+export { default as ProjectCard } from './ProjectCard.astro';
+export { default as ProjectGrid } from './ProjectGrid.astro';
